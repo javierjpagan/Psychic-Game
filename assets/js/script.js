c3584cbe3542c51f09a23396940829560889c75a
javierjpagan <9905163+javierjpagan@users.noreply.github.com>
2019-05-27 15:46:43 -0400

    Updated HTML & Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+
+//VARIABLES
+//Options to select.
+var letters = "aeiou";
+//Countdown of Guesses left
+var guessesLeft = 9;
+// Win & Losses counter
+var win = 0;
+var loss = 0;
+//What user guesses
+var lettersGuessed = [];
+//Random letter from computer
+var computerGuess = null;
+
+//FUNCTIONS
+//Random selection function
+var updatecomputerGuess = function() {
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+};
+
+//Updates Guesses left
+var updateguessesLeft = function() {
+    document.querySelector("#guessLeft").innerHTML = guessesLeft;
+};
+
+//Updates Guesses so far
+var updateGuessesSoFar = function() {
+    document.querySelector("#GuessesSoFar").innerHTML = lettersGuessed.join(", ");   
+};
+
+//Reset
+var reset = function() {
+    guessesLeft = 9;
+    lettersGuessed = [];
+    updatecomputerGuess();
+    updateguessesLeft();
+    updateGuessesSoFar();
+}
+
+
+updateguessesLeft();
+updateGuessesSoFar();
+
+
+document.onekeydown = function(event) {
+
+    //Turns letter input into lower case, then it adds to an array of letters already used and reduces number of guesses left
+    //Update Guesses left and Guesses so far on the page
+    var letters = event.key.toLowerCase();
+    lettersGuessed.push(letter);
+    guessesLeft--;
+    updateguessesLeft();
+    updateGuessesSoFar();
+
+   //If statement if the user guesses the correct letter, the user wins the game
+    if (letters === computerGuess) {
+        win++;
+        document.querySelector("#wins").innerHTML = win;
+        reset();
+
+    }
+
+    //If statement if the user reaches 0 tries out of 9, the user loses the game
+    if (guessesLeft === 0) {
+        loss++;
+        document.querySelector("#losses").innerHTML = loss;
+        reset();
+    }
+
+}
